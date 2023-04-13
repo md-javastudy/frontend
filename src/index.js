e518debefa8d0bd9b7a07318f702ab529018eec5
md_home <jlkmh82@msn.com>
2023-04-13 09:54:03 +0900

    [#][FIX] react 17 -> 18 방식 수정

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // React 요소
 const app = React.createElement(
-    'div',
-    { className: 'app' },
-    'React App 매뉴얼 구성'
+  "div",
+  { className: "app" },
+  "React App 매뉴얼 구성"
 );
 // DOM 노드
-const rootNode = document.getElementById('root');
+const rootNode = ReactDOM.createRoot(document.getElementById("app"));
 // DOM 노드에 React 요소 렌더링
-ReactDOM.render(app, rootNode);
+rootNode.render(app);
