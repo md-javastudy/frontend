7fa53400763e065fd50d9e4cd1f26f8d79de7b73
md_home <jlkmh82@msn.com>
2023-07-09 12:51:07 +0900

    [FEAT] 유동적인 Link 사용_수정

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,15 @@
-import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const router = useRouter();
   return (
     <div>
-      <h2>Link to 'Poteto' Page</h2>
-      <Link href="/vegetable/poteto">Move to '/poteto'</Link>
+      <button type={"button"} onClick={() => router.push("/tomato")}>tomato로 가기</button>
+      <p>이름</p>
+      <input value={name} onChange={(e) => setName(e.target.value)} style={{marginRight: "12px"}}/>
+      <button type={"button"} onClick={() => router.push(`/vegetable/${name}`)}>{name}으로 가기</button>
     </div>
   );
 };
--- pages/vegetable/[name].jsx
+++ pages/vegetable/[name].jsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 const name = () => {
-  const router = useRouter();
-  console.log(router);
+  const {query} = useRouter();
+
   return (
     <div>
-      <h2>Hello!!</h2>
+      <h2>Hello!! {query.name}</h2>
       <Link href="/">Move to '/'</Link>
     </div>
   );
