fb85a0d071242f9e5e2b9d4ccedc8d75816493a8
md_home <jlkmh82@msn.com>
2023-07-09 14:30:00 +0900

    [FEAT] github 정보 가지고 오기 (getServerSideProps 활용)

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
-import { useRouter } from "next/router";
+import React, {useState} from "react";
+import Link from "next/link";
 
 const App = () => {
-  const [name, setName] = useState("");
-  const router = useRouter();
+  const [username, setUsername] = useState("");
   return (
     <div>
-      <button type={"button"} onClick={() => router.push("/tomato")}>tomato로 가기</button>
-      <p>이름</p>
-      <input value={name} onChange={(e) => setName(e.target.value)} style={{marginRight: "12px"}}/>
-      <button type={"button"} onClick={() => router.push(`/vegetable/${name}`)}>{name}으로 가기</button>
+      <label>
+        username
+        <input value={username} onChange={(e) => setUsername(e.target.value)}/>
+      </label>
+      <p>{username} 깃허브 검색하기</p>
+      <Link href={`/users/${username}`}>검색하기</Link>
     </div>
   );
 };
