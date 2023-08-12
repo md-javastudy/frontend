53d511f39fc57bffabea45e5d2ac4c43e84129f5
md82 <md82@tistory.com>
2023-08-13 00:02:57 +0900

    [#][FEAT] 2.4.2 add react-icons

--- pages/users/[name].jsx
+++ pages/users/[name].jsx
@@ -1,68 +1,31 @@
-import css from "styled-jsx/css"
+import Profile from "../../components/Profile";
 import fetch from "isomorphic-unfetch";
-
-const style = css`
-  .profile-box {
-    width: 25%;
-    max-width: 272px;
-    margin-right: 26px;
-  }
-  .profile-image-wrapper {
-    width: 100%;
-    border: 1px solid #e1e4e8;
-  }
-  .profile-image-wrapper .profile-image {
-    display: block;
-    width: 100%;
-  }
-  .profile-username {
-    margin: 0;
-    padding-top : 16px;
-    font-size: 26px;
-  }
-  .profile-user-login {
-    margin: 0;
-    font-size: 20px;
-  }
-  .profile-user-bio {
-    margin: 0;
-    padding-top: 16px;
-    font-size: 14px;
-  }
-`;
 
 const name = ({ user }) => {
   if(!user) {
-    return null;
+    return 'null';
   }
   return (
-    <>
-    <div className="profile-box">
-      <div className="profile-image-wrapper">
-        <img className="profile-image" src={user.avatar_url} alt={`${user.name} 프로필 이미지`} />
-      </div>
-      <h2 className="profile-username">{user.name}</h2>
-      <p className="profile-user-login">{user.login}</p>
-      <p className="profile-user-bio">{user.bio}</p>
-    </div>
-    <style jsx>{style}</style>
-    </>
+      <>
+        <Profile user={user} />
+      </>
   );
 };
 
 export const getServerSideProps = async ({ query }) => {
-  const { name } = query;
-  try {
-    const res = await fetch(`https://api.github.com/users/${name}`);
-    if(res.status === 200) {
-      const user = await res.json();
-      return { props: { user }};
+    const { name } = query;
+    try {
+        const res = await fetch(`https://api.github.com/users/${name}`,
+            {headers: {Authorization: "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",},});
+        if(res.status === 200) {
+            const user = await res.json();
+            return { props: { user }};
+        }
+        return { props: {}};
+    } catch (e) {
+        console.log(e);
+        return { props: {}};
     }
-    return { props: {}};
-  } catch (e) {
-    console.log(e);
-    return { props: {}};
-  }
 };
 
 export default name;
